dd0ff5cc377c6f1c0f435815d36aae3cc2e1f097
Tarun rk <tarunrk2002@gmail.com>
2024-11-06 20:18:18 +0530

    till adfuller

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import pandas as pd
-excel_file_path = "WalmartData2022To2024.xlsx"
-data = pd.read_excel(excel_file_path)
-data['Date']=pd.to_datetime(data['week_end_date'])
-data = data.drop(['week_end_date', ], axis=1)
+import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
 
-data = data.sort_values(by='Date')
+excel_file_path = "perrin-freres-monthly-champagne-.csv"
+data = pd.read_csv(excel_file_path)
 
-data.to_csv('WalmartData2022To2024_sorted.csv',index=False)   
+#changing the names for the columns
+data.columns=['Month','Sales']
+
+#removing the rows with missing values with drop method
+data.drop(105, axis=0, inplace=True)
+data.drop(106, axis=0, inplace=True)
+
+#sometimes the data is not in the right format so we need to convert it . here, we are converting it to datetime 
+data['Month'] = pd.to_datetime(data['Month'])
+
+
+data.set_index('Month', inplace=True)
+# data.plot()
+
+# plt.show()
+
+test_result = adfuller(data['Sales'])
+
+print(test_result)
+
+
+
 
 
 
