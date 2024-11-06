01c41da2f622bb236f3d8b0e1d9d0b2fab1ca147
Tarun rk <tarunrk2002@gmail.com>
2024-11-06 13:25:28 +0530

    added date and removed week_end_date

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pandas as pd
 excel_file_path = "WalmartData2022To2024.xlsx"
 data = pd.read_excel(excel_file_path)
+data['Date']=pd.to_datetime(data['week_end_date'])
+data = data.drop(['week_end_date', ], axis=1)
+
+data = data.sort_values(by='Date')
+
+data.to_csv('WalmartData2022To2024_sorted.csv',index=False)   
+
+
 
 
 
